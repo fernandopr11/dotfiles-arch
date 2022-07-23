2c011f1acd372d4ba89da3aaf925eb1749115fcf
Fernando Paucar <fernandopaucar149@gmai.com>
2022-07-22 22:08:40 -0500

    Updated config folder of fish and qtile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,18 @@ set fish_greeting ""
 # Aliases
 
 alias grep "grep --color=auto"
+alias cat "bat --style=plain --paging=never"
 alias ls "exa --group-directories-first"
 alias tree "exa -T"
+alias off "shutdown now"
+alias up "yay -Syu"
+alias fpm "/home/fernando/.local/share/gem/ruby/3.0.0/bin/fpm"
+alias down "git p -f origin HEAD^:main"
+
+alias st "/home/fernando/scripts/./test-suite.sh"
+
+alias bdd "cd /home/fernando/Escritorio/Universidad/GestionBDD"
 
 # Prompt
+
 starship init fish | source
